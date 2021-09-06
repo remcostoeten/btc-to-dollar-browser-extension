5f8a6d0ea8fd9112b3112884988213a349b2b949
remcostoeten <remco@tickles.nl>
2021-09-07 00:20:50 +0200

    Peter fix.

--- init.js
+++ init.js
@@ -1,25 +1,22 @@
-(function () {
+$(document).ready(function () {
+    textBox();
+
     function textBox() {
         $.getJSON("https://api.coindesk.com/v1/bpi/currentprice/usd.json",
             function (data) {
-                var content = document.querySelector('.ant-table-cell span')
-                var scrapedBTCValue = content.textContent;
-                var btcValue = scrapedBTCValue;
-                var myParent = document.querySelector('.ant-table-row-level-0')
-                var box = document.createElement("div");
-                var amountInBtc = scrapedBTCValue; //convert 0.005 btc to usd
-                var exchangeRate = parseInt(data.bpi.USD.rate_float);
-                var amount = amountInBtc * exchangeRate;
-                box.id = "dollar-box";
-                box.classList = "btc-dollar-wrapper wrapper";
-                box.innerHTML = scrapedBTCValue + " = " + amount.toFixed(2) + "$";
-                myParent.appendChild(box);
-                console.log(amount.toFixed(4));
-                setTimeout(() => {
-                }, 3000)
-
+                $('tr.ant-table-row').each(function () {
+                    var scrapedBTCValue = $(this).find('.ant-table-cell span').text();
+                    var btcValue = scrapedBTCValue;
+                    var box = document.createElement("div");
+                    var amountInBtc = scrapedBTCValue; //convert 0.005 btc to usd
+                    var exchangeRate = parseInt(data.bpi.USD.rate_float);
+                    var amount = amountInBtc * exchangeRate;
+                    box.id = "dollar-box";
+                    box.classList = "btc-dollar-wrapper wrapper";
+                    box.innerHTML = scrapedBTCValue + " = " + amount.toFixed(2) + "$";
+                    $(this).append(box);
+                    console.log(amount.toFixed(4));
+                });
             });
     }
-
-    textBox();
-})();
+});
