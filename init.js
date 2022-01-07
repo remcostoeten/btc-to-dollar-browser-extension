cfa9e6f918244fbce5880810cd041c059d86702a
Remco Stoeten <remcostoeten@hotmail.com>
2022-01-07 20:49:01 +0100

    Finalize btc converter to dollar in pnl modal

--- init.js
+++ init.js
@@ -9,7 +9,7 @@ let exchangeRate = 1;
 // document.getElementsByClassName('trade-assets__head-title')[0].appendChild(strong);
 
 
-//function to convert currency on individual rows, triggered by MutationObserver and after exchange rate is fetched.
+// function for PNL page to convert currency on individual rows, triggered by MutationObserver and after exchange rate is fetched.
 function convertBtcToUsd(row) {
     const scrapedBTCValue = row.querySelector('.ant-table-cell span').textContent;
     const box = document.createElement('div');
@@ -30,12 +30,31 @@ function convertBtcToUsd(row) {
     row.appendChild(box);
     console.log(amount.toFixed(4));
 }
+// function for modal popup pnl page.
+const checkExist = setInterval(function () {
+
+    const btcScraped = Number.parseFloat(document.querySelector('#modal-root .tpsl-modal__tips-desc').innerText.match(/[0-9,.]+\sBTC/)[0].split(/\s/)[0]) //  Scrape BTC value from class 
+
+    const btcFinal = btcScraped * exchangeRate; // convert BTC to dollars
+
+    if (document.querySelector('#modal-root .tpsl-modal__tips-desc') != null) {
+        if (document.querySelector('#modal-root .tpsl-modal__tips-desc').innerHTML.length) {
+            const extraBox = document.createElement('div');
+            extraBox.classList.add('btc-to-dollar-box');
+            extraBox.classList.add('btc-to-dollar-box');
+            document.getElementsByClassName('tpsl-modal__tips-desc')[0].appendChild(extraBox);
+
+            extraBox.innerHTML = btcScraped + "BTC = " + btcFinal.toFixed(2) + '$';
+            clearInterval(checkExist);
+        }
+    }
+}, 100); // check every 100ms
 
 //create observer that triggers convert code when new rows are added
 const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
         mutation.addedNodes.forEach((addedNode) => {
-            if(exchangeRateLoaded && addedNode.nodeName === 'TR' && addedNode.classList.contains('ant-table-row')) {
+            if (exchangeRateLoaded && addedNode.nodeName === 'TR' && addedNode.classList.contains('ant-table-row')) {
                 convertBtcToUsd(addedNode);
             }
         });
@@ -49,7 +68,7 @@ observer.observe(document, {
 
 //load exchange rate from Coindesk API
 fetch('https://api.coindesk.com/v1/bpi/currentprice/usd.json').then((response) => {
-    if(response.status != 200) {
+    if (response.status != 200) {
         console.error('Coindesk API returned non-success code')
         console.log(response);
         return;
@@ -61,29 +80,3 @@ fetch('https://api.coindesk.com/v1/bpi/currentprice/usd.json').then((response) =
         document.querySelectorAll('tr.ant-table-row').forEach((row) => convertBtcToUsd(row)) //convert already loaded rows now that we know the exchange rate
     })
 })
-
-var checkExist = setInterval(function() {
-    
-    var btcScraped = Number.parseFloat(document.querySelector('#modal-root .tpsl-modal__tips-desc').innerText.match(/[0-9,.]+\sBTC/)[0].split(/\s/)[0])  //  Scrape btc value from class 
-
-    if (document.querySelector('#modal-root .tpsl-modal__tips-desc') != null) {
-        if ( document.querySelector('#modal-root .tpsl-modal__tips-desc').innerHTML.length ) {
-            console.log( "Exists!" );
-            document.getElementById("modal-root").className = "remco"
-
-            var testTest = "test123"
-
-            var testBox = document.createElement('div');
-            testBox.innerHTML = "test123";
-
-            var extraBox = document.createElement('div');
-            extraBox.innerHTML = btcScraped + "BTC = " ;
-            extraBox.classList.add('btc-to-dollar-box');
-            document.getElementsByClassName('tpsl-modal__tips-desc')[0].appendChild(extraBox);
-
-            clearInterval( checkExist );
-        }
-    }
-}, 100); // check every 100ms
-
-// document.getElementById("rc-tabs-0-tab-inverse").style.color = "red";
